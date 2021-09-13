d2ec172ccd6cc72a1101716df492f2270669d144
pmg41 <pragnesh30gajera@gmail.com>
2021-09-13 13:10:02 +0530

    -fix issue

--- app/src/main/java/com/valora/promotion/MainActivity.kt
+++ app/src/main/java/com/valora/promotion/MainActivity.kt
@@ -15,8 +15,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initView()
-
-
     }
 
     fun initView() {
